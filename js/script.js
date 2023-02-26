c492e1a94488be1b33da3590d0caf3fe50ead25c
Carlos Robles <chrobles@pm.me>
2023-02-26 17:49:32 -0500

    Initial Three.js scence, camera and renderer created in script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 function init() {
 
+// Create Scene
+const scene = new THREE.Scene();
 
+// Create Camera 
+const camera = new THREE.PerspectiveCamer(
+  75, // FOV
+  window.innerWidth / window.innerHeight, // Aspect Ratio
+  0.1, // Near
+  1000 // Far
+)
 
+// create Renderer 
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight);
+
+document.body.appendChild( renderer.domElement);
 
 
 
