b82142df6075e65abf4035304991019ada39e8e6
Carlos Robles <chrobles@pm.me>
2023-02-27 14:21:40 -0500

    Cube shape created and rendered.

--- js/script.js
+++ js/script.js
@@ -1,41 +1,42 @@
+let scene;
+let camera;
+let renderer;
+
 function init() {
+  // Create Scene
+  scene = new THREE.Scene();
 
-// Create Scene
-const scene = new THREE.Scene();
+  // Create Camera
+  camera = new THREE.PerspectiveCamera(
+    75, // FOV
+    window.innerWidth / window.innerHeight, // Aspect Ratio
+    0.1, // Near
+    1000 // Far
+  );
 
-// Create Camera 
-const camera = new THREE.PerspectiveCamer(
-  75, // FOV
-  window.innerWidth / window.innerHeight, // Aspect Ratio
-  0.1, // Near
-  1000 // Far
-)
+  // create Renderer
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-// create Renderer 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight);
-
-document.body.appendChild( renderer.domElement);
-
-createShape();
+  document.body.appendChild(renderer.domElement);
+  createShape();
+  animate();
 }
 
 function createShape() {
-  const geometry = new THREE.BoxGeometry( 1,1,1 );
-  const material = THREE.MeshBasicMaterial({
-    color: 0x00ff00
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({
+    color: 0x00ff00,
   });
-  const cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
 
+  camera.position.z = 5;
 }
 
-
-
-
-
-
-
-
+function animate() {
+  requestAnimationFrame( animate );
+  renderer.render( scene, camera );
+}
 
 init();
